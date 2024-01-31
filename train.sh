eefec51876bede071451be23492dd7ec7c02c6d7
YiningHong <hyn0027diana@gmail.com>
2024-02-01 00:50:02 +0800

    ssh agent

--- train.sh
+++ train.sh
@@ -6,3 +6,9 @@ torchrun --nnodes 1 --nproc_per_node 8  examples/finetuning.py \
     --fsdp_config.pure_bf16 \
     --output_dir /workspace/llama-recipes/training-output/silver_2.0_0 \
     --num_epochs 12 --lr 1e-4 > log.txt 2>&1
+
+git add .
+
+git commit -m "llama-2.0-0"
+
+git push origin main
