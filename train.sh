45c0cc93b2104efa9ed0f957a2ad6f5899e973d3
YiningHong <hyn0027diana@gmail.com>
2024-02-01 00:51:15 +0800

    llama-2.0-0

--- train.sh
+++ train.sh
@@ -1,11 +1,11 @@
-torchrun --nnodes 1 --nproc_per_node 8  examples/finetuning.py \
-    --dataset "AMR2_dataset" \
-    --enable_fsdp --use_peft --peft_method lora \
-    --model_name /workspace/ModelWeights/llama-2-7b-hf \
-    --peft_model /workspace/llama-recipes/training-output/silver_0 \
-    --fsdp_config.pure_bf16 \
-    --output_dir /workspace/llama-recipes/training-output/silver_2.0_0 \
-    --num_epochs 12 --lr 1e-4 > log.txt 2>&1
+# torchrun --nnodes 1 --nproc_per_node 8  examples/finetuning.py \
+#     --dataset "AMR2_dataset" \
+#     --enable_fsdp --use_peft --peft_method lora \
+#     --model_name /workspace/ModelWeights/llama-2-7b-hf \
+#     --peft_model /workspace/llama-recipes/training-output/silver_0 \
+#     --fsdp_config.pure_bf16 \
+#     --output_dir /workspace/llama-recipes/training-output/silver_2.0_0 \
+#     --num_epochs 12 --lr 1e-4 > log.txt 2>&1
 
 git add .
 
